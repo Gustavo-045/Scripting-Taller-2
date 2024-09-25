e4c9e5a9b11fe9cb87aac105cf9efd3612f12d93
Sebastian Torres Rios <143814417+sebastr008@users.noreply.github.com>
2024-09-24 20:09:47 -0500

    logger

---
+++ Singleton/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+
+    {
+        private static Logger _Instance;
+        private string logFilePath = "log.txt"; // Ruta del archivo de log
+
+
+
+        // Método para escribir mensajes en el archivo de log
+        public void Log(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+
+
+
+
+    }
+
+}
+
+
+
+
+
