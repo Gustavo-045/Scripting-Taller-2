223074f7f3618a45dbc25328776018234154ae9b
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 21:00:18 -0500

    Add files via upload

---
+++ Adapter/AdaptadorDinosaurio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class AdaptadorDinosaurio : IPajaro
+    {
+        private Dinosaurio _dinosaurio;
+
+        public AdaptadorDinosaurio(Dinosaurio dinosaurio)
+        {
+            _dinosaurio = dinosaurio;
+        }
+
+        public void Cantar()
+        {
+            _dinosaurio.Rugir();
+        }
+    }
+
+}
