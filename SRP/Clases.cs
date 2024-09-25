4ea41e1254cb2cfccf3eb8e7841efe60613670b4
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 20:56:26 -0500

    actualización Clases.cs

--- SRP/Clases.cs
+++ SRP/Clases.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +22,14 @@ namespace SRP
         }
     }
 
-
+    // Almacenamiento.cs
+    public class Almacenamiento
+    {
+        public void GuardarUsuario(string nombre)
+        {
+            Console.WriteLine($"Usuario {nombre} guardado en archivo.");
+        }
+    }
 
 }
+
