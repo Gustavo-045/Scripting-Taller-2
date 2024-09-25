ecd32af3b86b2c6d6627e888be1ce17bd32b7108
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 20:59:24 -0500

    Add files via upload

---
+++ SRP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    // Program.cs
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Juan";
+            Usuario usuario = new Usuario(nombre);
+            usuario.CrearUsuario();
+
+            Almacenamiento almacenamiento = new Almacenamiento();
+            almacenamiento.GuardarUsuario(nombre);
+        }
+    }
+
+}
