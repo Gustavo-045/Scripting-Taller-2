524a2b398740bb43882cdca6f46bf62e11ba3efc
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 21:02:09 -0500

    Add files via upload

---
+++ Adapter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dinosaurio dino = new Dinosaurio();
+            IPajaro pajaroAdaptado = new AdaptadorDinosaurio(dino);
+
+            // El dinosaurio puede "cantar" (rugir) a través del adaptador
+            pajaroAdaptado.Cantar(); // Salida: ¡Ruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu!
+        }
+    }
+}
+
