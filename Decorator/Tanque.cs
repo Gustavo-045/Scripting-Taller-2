4915d0b78f56ded927923dedf7a63efd6561669a
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 21:03:12 -0500

    Add files via upload

---
+++ Decorator/Tanque.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    // Tanque.cs
+    public class Tanque
+    {
+        public virtual string Descripcion()
+        {
+            return "Tanque de la Segunda Guerra Mundial";
+        }
+
+        public virtual double Costo()
+        {
+            return 10000; // Costo base del tanque
+        }
+    }
+
+
+}
