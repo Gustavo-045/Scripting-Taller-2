d4da015dd1002e37ebeb97bd48465dc3df4ad525
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 21:03:31 -0500

    Add files via upload

---
+++ Decorator/TanqueConBlindaje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    // TanqueConBlindaje.cs
+    public class TanqueConBlindaje : Tanque
+    {
+        private Tanque _tanque;
+
+        public TanqueConBlindaje(Tanque tanque)
+        {
+            _tanque = tanque;
+        }
+
+        public override string Descripcion()
+        {
+            return _tanque.Descripcion() + ", con blindaje adicional";
+        }
+
+        public override double Costo()
+        {
+            return _tanque.Costo() + 5000; // Costo adicional por el blindaje
+        }
+    }
+
+
+}
