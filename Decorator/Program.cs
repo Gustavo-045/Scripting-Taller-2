c47fe7bdd96195084aa1f4b58e2b4826a2f47f81
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 21:02:59 -0500

    Add files via upload

---
+++ Decorator/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program.cs
+using Decorator;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear un tanque básico
+        Tanque tanque = new Tanque();
+        Console.WriteLine($"{tanque.Descripcion()} - Costo: ${tanque.Costo()}");
+
+        // Decorar el tanque con blindaje
+        Tanque tanqueBlindado = new TanqueConBlindaje(tanque);
+        Console.WriteLine($"{tanqueBlindado.Descripcion()} - Costo: ${tanqueBlindado.Costo()}");
+    }
+}
