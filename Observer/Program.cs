70eec453fb93ea7f432d37f1fc78958fb7f79172
Sebastian Torres Rios <143814417+sebastr008@users.noreply.github.com>
2024-09-24 20:17:49 -0500

    Update Program.cs

---
+++ Observer/Program.cs
@@ -0,0 +1,17 @@
+using Observer;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear una instancia del dictador
+        Dictador dictador = new Dictador();
+
+        // Crear ciudadanos
+        Citizen ciudadano1 = new Citizen("Juan");
+        Citizen ciudadano2 = new Citizen("Maria");
+
+
+    }
+}
