0f1dc77733717d05f513ccded21dae133ae2bcda
Sebastian Torres Rios <143814417+sebastr008@users.noreply.github.com>
2024-09-24 20:16:51 -0500

    Update Dictador.cs

---
+++ Observer/Dictador.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Observer;
+
+public class Dictador
+{
+    private List<IObserver> ciudadanos = new List<IObserver>();
+    private string mensaje;
+
+    // Método para agregar ciudadanos
+    public void Subscribe(IObserver ciudadano)
+    {
+        ciudadanos.Add(ciudadano);
+    }
+
+    // Método para eliminar ciudadanos
+    public void Unsubscribe(IObserver ciudadano)
+    {
+        ciudadanos.Remove(ciudadano);
+    }
+
+  
+  
+}
