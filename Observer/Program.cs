5efcd1bb907a5181b8610f0f1267e4fad1db8af1
Sebastian Torres Rios <143814417+sebastr008@users.noreply.github.com>
2024-09-24 20:18:17 -0500

    Update Program.cs

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -12,6 +12,19 @@ class Program
         Citizen ciudadano1 = new Citizen("Juan");
         Citizen ciudadano2 = new Citizen("Maria");
 
+        // Suscribir ciudadanos al dictador
+        dictador.Subscribe(ciudadano1);
+        dictador.Subscribe(ciudadano2);
+
+        // Cambiar el mensaje y notificar a los ciudadanos
+        dictador.ChangeMensaje("¡El dictador ha declarado la ley marcial!");
+
+        // Un ciudadano se desuscribe
+        dictador.Unsubscribe(ciudadano1);
+
+        // Cambiar el mensaje nuevamente
+        dictador.ChangeMensaje("¡El dictador ha impuesto nuevas restricciones!");
+
 
     }
 }
