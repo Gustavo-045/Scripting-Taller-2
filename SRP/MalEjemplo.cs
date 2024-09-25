00ba87eaee75f3f8ea11f2a7db1d479bcda22cd2
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-09-24 20:57:01 -0500

    Add files via upload

---
+++ SRP/MalEjemplo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class MalEjemplo
+{
+    public void CrearUsuario(string nombre)
+    {
+        // Lógica para crear un usuario
+        Console.WriteLine($"Usuario {nombre} creado.");
+
+        // Lógica para guardar el usuario en un archivo
+        GuardarUsuarioEnArchivo(nombre);
+    }
+
+    private void GuardarUsuarioEnArchivo(string nombre)
+    {
+        Console.WriteLine($"Usuario {nombre} guardado en archivo.");
+    }
+}
